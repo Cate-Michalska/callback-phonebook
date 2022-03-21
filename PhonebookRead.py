ef2a79429b1d9e76495009dce216fef43167c8d4
taehoonyun <mn9069@naver.com>
2022-03-22 06:11:35 +0900

    second commit

--- PhonebookRead.py
+++ PhonebookRead.py
@@ -2,24 +2,28 @@ from distutils.util import execute
 import pymysql
 
 conn, cur = None, None
-data1, data2 = "", ""
+data1, data2, data3 = "", "", ""  # variable for name and number
 row = None
+
+# connect mysql and Python
 
 conn = pymysql.connect(host='127.0.0.1', user='root',
                        password='@MNmn0065', db='PhoneBookDB')
 cur = conn.cursor()
+
+# push sql code to mysql
 cur.execute("SELECT * FROM userTable")
 
-print("USER NAME   PHONENUMBER")
+print("USER NAME   PHONENUMBER   BIRTHINFO")
 print("------------------------")
 
 while(True):
-    row = cur.fetchone()
+    row = cur.fetchone()  # get sql row data from mysql.
     if row == None:
         break
-    data1, data2 = row
+    data1, data2, data3 = row  # put sql row data to data1, data2
 
-    print("%15s  %d" % (data1, data2))
+    print("%15s  %d  %d" % (data1, data2, data3))
 
 
-conn.close()
+conn.close()  # close the connection
