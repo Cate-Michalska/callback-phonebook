9239c7c1a224fc2215a7aa7ff3d3d93f6b1ad974
taehoonyun <mn9069@naver.com>
2022-03-22 06:11:35 +0900

    second commit

--- PhonebookInsert.py
+++ PhonebookInsert.py
@@ -2,27 +2,26 @@ from distutils.util import execute
 import pymysql
 
 conn, cur = None, None
-Me, MyNumber, data1, data2 = "", "", "", ""
+data1, data2, data3 = "", "", ""
 sql = ""
 
+# connect mysql and Python
 conn = pymysql.connect(host='127.0.0.1', user='root',
                        password='@MNmn0065', db='PhoneBookDB')
 cur = conn.cursor()
-sql = "CREATE TABLE IF NOT EXISTS userTable (USERNAME char(15), PHONENUMBER int)"
+
+# create table with sql code.
+sql = "CREATE TABLE IF NOT EXISTS userTable (USERNAME char(15), PHONENUMBER int, BIRTHDATE int)"
 cur.execute(sql)
-# if Me == "":
-#     Me = input("Inser my name ==> ")
-#     MyNumber = input("My phone number ==> ")
-#     cur.execute("INSERT INTO userTable VALUES('"+Me +
-#                 "',"+MyNumber+")")
-# else:
+
 while(True):
-    data1 = input("User NAME ==>")
-    if data1 == "":
+    data1 = input("User NAME ==>")  # input name in data1
+    if data1 == "":  # if input is nothing, the loop breaks
         break
-    data2 = input("User PhoneNumber==>")
+    data2 = input("User PhoneNumber==>")  # input number in data2
+    data3 = input("User Birthday info==>")  # input birth info
     sql = "INSERT INTO userTable VALUES('"+data1 + \
-        "',"+data2+")"
+        "',"+data2+","+data3+")"
     cur.execute(sql)
 
 
