ee5e3204f903f5dd8e6d5155c930108a3d39f120
Davidemenike <59932349+Davidemenike@users.noreply.github.com>
2022-04-04 14:24:36 -0500

    Add files via upload

--- Tkinkerphone.py
+++ Tkinkerphone.py
@@ -4,7 +4,9 @@ from msilib.schema import ListBox
 from struct import pack
 from textwrap import fill
 import tkinter as tk
-from turtle import bgcolor, width
+from turtle import bgcolor, color, width
+from venv import create
+
 
 
 #the main window to be create
@@ -15,12 +17,52 @@ w = tk.Canvas(master, width = 350, height=600)
 w.configure(bg = 'white')
 
 
+
+
 #add contact bar and search bar with button for creating new contacts
+def contact():
+    new = tk.Toplevel(w)
+    new.geometry("350x450")
+    
+    new.title("Add Contact")
+    
+    newContact_sect = tk.Canvas(new, width="350", background="#7ED957", height = 120)
+    newContact_sect.create_text(175, 60, text = "ADD CONTACT", fill="white",font = "Helvetica 22 bold")
+
+
+    #FIRST NAME AND LAST NAME SECTION
+    newContact_sect2 = tk.Canvas(new, width="350", background="white", height = 320)
+
+    newContact_sect2.create_text(65, 40, text="First Name", fill="grey",font= "Helvetica 15 bold" )
+    firstName_entry = tk.Entry(new, width="20", background="white", foreground="black",highlightbackground="grey",highlightcolor='grey',highlightthickness=4).place(x = 14, y = 180)
+    
+
+    newContact_sect2.create_text(260, 40, text='Last Name', fill = "grey", font="Helvetica 15 bold")
+    lastName_entry = tk.Entry(new, width='20', background='white', foreground="black", highlightcolor="grey", highlightbackground='grey',highlightthickness=4).place(x=200, y= 180)
+
+
+    newContact_sect2.create_text(90, 110, text = "Phone Number", fill = 'grey', font='Helvetica 15 bold')
+    phoneNumber_entry = tk.Entry(new, background='white', foreground="black", highlightcolor="grey", highlightbackground='grey',highlightthickness=4).place(x = 14, y = 250, width = 300)
+
+    newContact_sect2.create_text(80, 180, text = "BIRTHDAY", fill = "grey", font = "Helvetica 15 bold")
+    birthday_entry = tk.Entry(new, background='white', foreground="black", highlightcolor="grey", highlightbackground='grey',highlightthickness=4).place(x = 14, y = 320,width = 300)
+
+    Submit_button = tk.Button(new, width = '20',text="Enter",bg= '#008037',height = 2).place(x= 14, y = 355)
+
+    newContact_sect.pack()
+    newContact_sect2.pack()
+
+
+
+
+
+
 new_w = tk.Canvas(master, width= 350, height = 60 )
 new_w.configure(bg = "#7ed957")
 # green_label = tk.Label(new_w, text="Add Contact",background="green",foreground="white").place(x =160, y = 20)
-add_button = tk.Button(new_w, text = "Add contact", activebackground="green", activeforeground="red",height=1).place(x = 160, y = 20)
-enter_label = tk.Entry(new_w, width="20",background="white", foreground="blue").place(x = 30, y=20)
+add_button = tk.Button(new_w, text = "Add contact", activebackground="green", activeforeground="red",height=1, command=contact).place(x = 160, y = 20)
+enter_label = tk.Entry(new_w, width="20",background="white", foreground="blue", text = "search").place(x = 30, y=20)
+
 
 
 
@@ -35,9 +77,11 @@ Me_sect.create_line(22,65,350,65, fill="green", width=1)
 
 
 #contacts section that will have list of contacts 
-contacts_sect = tk.Canvas(master,width = 350, height = 100)
+contacts_sect = tk.Canvas(master,width = 350, height = 500)
 contacts_sect.create_text(90, 20, text='MY CONTACTS', fill = "green", font = "Helvetica 15 bold")
 contacts_sect.create_line(22,35,350,35, fill="green", width=1)
+
+# if add_button(is)
 
 
 
