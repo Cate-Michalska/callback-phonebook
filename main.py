5180049e70cf14c2273045139000fa8673640a46
Davidemenike <59932349+Davidemenike@users.noreply.github.com>
2022-04-13 15:28:25 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from distutils.cmd import Command
 import PhonebookInsert as Inserting
 import PhonebookDelete as Delete
 import PhonebookRead as Read
@@ -6,13 +7,17 @@ from distutils.util import execute
 
 # initial imports that will be needed
 from cgitb import text
+from fileinput import close
 from msilib.schema import ListBox
+from os import kill
+
 from struct import pack
 from textwrap import fill
 import tkinter as tk
+from tkinter.ttk import Entry
 from turtle import bgcolor, color, width
 from venv import create
-
+import os
 
 # the main window to be create
 # eventually we will use classes and self._init_ for this one
@@ -23,6 +28,8 @@ w.configure(bg='white')
 
 
 # add contact bar and search bar with button for creating new contacts
+
+
 def contact():
     new = tk.Toplevel(w)
     new.geometry("350x450")
@@ -61,19 +68,138 @@ def contact():
     birthday_entry = tk.Entry(new, background='white', foreground="black", highlightcolor="grey",
                               highlightbackground='grey', highlightthickness=4)
     birthday_entry.place(x=14, y=320, width=300)
+    if len(birthday_entry.get()) == 0:
+        birthday_entry.insert(0, 0)
     # button run insert function and convey each entry data
     Submit_button = tk.Button(
-        new, width='20', text="Enter", bg='#008037', height=2, command=lambda: Inserting.insertData(firstName_entry, lastName_entry, phoneNumber_entry, birthday_entry)).place(x=14, y=355)
-
+        new, width='20', text="Enter", bg='#008037', height=2, command=lambda: [Inserting.insertData(firstName_entry, lastName_entry, phoneNumber_entry, birthday_entry), makeNameList(), new.destroy()])
+    Submit_button.place(x=14, y=355)
     newContact_sect.pack()
     newContact_sect2.pack()
+
+
+def displayContact(Contact, Phone_num, birthdate):
+    new2 = tk.Toplevel(w)
+    new2.geometry = ("350x450")
+    display_sect = tk.Canvas(
+        new2, width="350", background="#7ED957", height=120)
+    display_sect.create_text(
+        175, 60, text=(Contact), fill="white", font="Helvetica 22 bold")
+
+    display_sect2 = tk.Canvas(
+        new2, width="350", background="white", height=320)
+
+    display_sect2.create_text(
+        90, 40, text="PHONE NUMBER", fill='grey', font="Helvetica 15 bold")
+    phoneNumber_entry = tk.Entry(display_sect2, text=Contact, background='white', foreground="black", highlightcolor="grey",
+                                 highlightbackground='grey', highlightthickness=4, textvariable="contacts_Phone")
+    phoneNumber_entry.place(x=40, y=70, width=200)
+    phoneNumber_entry.delete(0, END)
+    phoneNumber_entry.insert(0, Phone_num)
+    phoneNumber_entry.config(state='readonly')
+
+    display_sect2.create_text(70, 120, text="BIRTHDAY",
+                              fill="grey", font="Helvetica 15 bold")
+    birthday_entry = tk.Entry(display_sect2, background="white",
+                              highlightbackground="grey", highlightthickness=4, foreground="black")
+    birthday_entry.place(x=40, y=150, width=200)
+    birthday_entry.insert(0, birthdate)
+    birthday_entry.config(state='readonly')
+
+    edit_contact = tk.Button(new2, text="EDIT CONTACT", fg="white", activebackground="green",
+                             bg='#008037', activeforeground="red", height=1, command=lambda Contact=Contact, Phone_num=Phone_num, birthdate = birthdate: [editContact(Contact, Phone_num,birthdate), makeNameList(), new2.destroy()])
+    edit_contact.place(x=40, y=350)
+    delete_contact = tk.Button(new2, text="DELETE CONTACT", fg="red", activebackground='green',
+                               bg='#008037', command=lambda Contact=Contact, Phone_num=Phone_num: [Delete.DeleteData(Contact, Phone_num), makeNameList(), new2.destroy()], activeforeground='red', height=1)
+    delete_contact.place(x=200, y=350)
+
+    display_sect.pack()
+    display_sect2.pack()
+
+def editContact(Contact, Phonenum, birthdate):
+    new = tk.Toplevel(w)
+    new.geometry("350x450")
+
+    new.title("Edit Contact")
+
+    newContact_sect = tk.Canvas(
+        new, width="350", background="#7ED957", height=120)
+    newContact_sect.create_text(
+        175, 60, text="EDIT CONTACT", fill="white", font="Helvetica 22 bold")
+
+    # FIRST NAME AND LAST NAME SECTION
+    newContact_sect2 = tk.Canvas(
+        new, width="350", background="white", height=320)
+
+    newContact_sect2.create_text(
+        65, 40, text="First Name", fill="grey", font="Helvetica 15 bold")
+    
+    a = Contact.split()
+
+    firstName_entry = tk.Entry(new, width="20", background="white", foreground="black",
+                               highlightbackground="grey", highlightcolor='grey', highlightthickness=4)
+    firstName_entry.insert(0, a[0])
+    
+    firstName_entry.place(x=14, y=180)
+
+    newContact_sect2.create_text(
+        260, 40, text='Last Name', fill="grey", font="Helvetica 15 bold")
+    lastName_entry = tk.Entry(new, width='20', background='white', foreground="black",
+                              highlightcolor="grey", highlightbackground='grey', highlightthickness=4)
+    lastName_entry.insert(0, a[1])
+    lastName_entry.place(x=200, y=180)
+
+    newContact_sect2.create_text(
+        90, 110, text="Phone Number", fill='grey', font='Helvetica 15 bold')
+    phoneNumber_entry = tk.Entry(new, background='white', foreground="black", highlightcolor="grey",
+                                 highlightbackground='grey', highlightthickness=4)
+    phoneNumber_entry.insert(0,Phonenum)
+    phoneNumber_entry.place(x=14, y=250, width=300)
+
+    newContact_sect2.create_text(
+        80, 180, text="BIRTHDAY", fill="grey", font="Helvetica 15 bold")
+    birthday_entry = tk.Entry(new, background='white', foreground="black", highlightcolor="grey",
+                              highlightbackground='grey', highlightthickness=4)
+    birthday_entry.insert(0, birthdate)
+    birthday_entry.place(x=14, y=320, width=300)
+    if len(birthday_entry.get()) == 0:
+        birthday_entry.insert(0, 0)
+    # button run insert function and convey each entry data
+    Submit_button = tk.Button(
+        new, width='20', text="Enter", bg='#008037', height=2, command=lambda: [Inserting.insertData(firstName_entry, lastName_entry, phoneNumber_entry, birthday_entry), makeNameList(), new.destroy()])
+    Submit_button.place(x=14, y=355)
+    newContact_sect.pack()
+    newContact_sect2.pack()
+
+
+
+
+def makeNameList():
+    contacts_Name = []
+    contacts_Phone = []
+    contacts_Birhinfo = []
+    i = 0
+    index = 0
+    contacts_Name, contacts_Phone, contacts_Birhinfo = Read.ReadData()
+    for names in contacts_Name:
+        Phone_num = str(contacts_Phone[index])
+        Contacts = contacts_Name[index]
+        Birthdate = contacts_Birhinfo[index]
+        list_name = names
+        list_name = tk.Button(contacts_sect, text=names, activebackground="green",
+                              activeforeground="red", height=1, command=lambda Phone_num=Phone_num, Contacts=Contacts, Birthdate=Birthdate: [displayContact(Contacts, Phone_num, Birthdate), list_name.pack_forget()], borderwidth=0)
+        list_name.place(x=20, y=40+i)
+        i += 20
+        index += 1
+        print(names)
 
 
 new_w = tk.Canvas(master, width=350, height=60)
 new_w.configure(bg="#7ed957")
 # green_label = tk.Label(new_w, text="Add Contact",background="green",foreground="white").place(x =160, y = 20)
 add_button = tk.Button(new_w, text="Add contact", activebackground="green",
-                       activeforeground="red", height=1, command=contact).place(x=160, y=20)
+                       activeforeground="red", height=1, command=contact)
+add_button.place(x=160, y=20)
 enter_label = tk.Entry(new_w, width="20", background="white",
                        foreground="blue", text="search").place(x=30, y=20)
 
@@ -95,11 +221,11 @@ contacts_sect.create_text(90, 20, text='MY CONTACTS',
                           fill="green", font="Helvetica 15 bold")
 # contacts section that will add list of contacts
 contacts_sect.create_line(22, 35, 350, 35, fill="green", width=1)
-i = 0
-for names in Read.ReadData():
-    contacts_sect.create_text(70, 60+i, text=names,
-                              fill="green", font="Helvetica 15 bold")
-    i += 50
+
+# create buttons
+button_dict = {}
+makeNameList()
+
 
 # if add_button(is)
 
